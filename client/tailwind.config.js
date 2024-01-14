d5088378b796101b206250da4e52c38cad9df086
Daniel Gutkin <dngutkin@gmail.com>
2024-01-13 21:27:43 -0500

    Add Tailwind CSS

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/*.js',
+    '/_componenets/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
