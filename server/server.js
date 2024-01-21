395e5dd280f4c1e0ef532c346182f24c507845bf
Daniel Gutkin <dngutkin@gmail.com>
2024-01-20 23:57:51 -0500

    Add cors for the Express server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
@@ -6,6 +7,8 @@ const userRoutes = require("./routes/userRoutes.js");
 
 const app = express();
 const port = 8080;
+
+app.use(cors({origin: 'http://localhost:3000'}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
