986b53b9d4481c3661b0dd8f1d9c895f0e7b7217
Daniel Gutkin <dngutkin@gmail.com>
2024-02-08 23:05:03 -0500

    Add login server call

--- client/app/login/page.js
+++ client/app/login/page.js
@@ -1,8 +1,48 @@
 'use client'
 
-import Link from 'next/link';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
 
 export default function Login() {
+
+    const [authError, setAuthError] = useState(false);
+    const [authErrorMessage, setAuthErrorMessage] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const router = useRouter();
+
+    async function handleLogin(e) {
+
+        e.preventDefault();
+
+        const data = {"email": email, "password": password};
+        const options = {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+        const url = "http://127.0.0.1:8080/login";
+        
+        await fetch(url, options)
+            .then((response) => {
+                console.log(response);
+                if (response.status == 200){
+                    router.push('/user');
+                } else {
+                    setAuthError(true);
+                    setAuthErrorMessage(response.text());
+                }
+            })
+            .catch((error) => {
+                throw new Error("Create Account Error");
+            });
+        
+    }
 
     return (
         
@@ -18,19 +58,23 @@ export default function Login() {
         
                         <div className="mb-4 mt-6">
                             <label htmlFor="email" className="block text-gray-600 text-sm font-semibold mb-2">Email</label>
-                            <input type="email" id="email" name="email" placeholder="Your email" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"/>
+                            <input type="email" id="email" name="email" placeholder="Your email" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" onChange={(e) => setEmail(e.target.value)}/>
                         </div>
 
                     
                         <div className="mb-6">
                             <label htmlFor="password" className="block text-gray-600 text-sm font-semibold mb-2">Password</label>
-                            <input type="password" id="password" name="password" placeholder="Your password" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"/>
+                            <input type="password" id="password" name="password" placeholder="Your password" className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500" onChange={(e) => setPassword(e.target.value)}/>
                         </div>
 
                     
-                        <button className="bg-dark-green text-white px-4 py-2 mt-6 rounded-md hover:bg-yinmn-blue focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-yinmn-blue">
-                            <Link href="/user">Log In</Link>
+                        <button className="bg-dark-green text-white px-4 py-2 mt-6 rounded-md hover:bg-yinmn-blue" onClick={handleLogin}>
+                            Log In
                         </button>
+
+                        {authError && 
+                        <p>{authErrorMessage}</p>
+                        }
 
                     </form>
 
