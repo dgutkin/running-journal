320aeacb822b681ad889e9b7ab4713550d3cc78f
Daniel Gutkin <dngutkin@gmail.com>
2024-02-08 21:34:26 -0500

    Add colour pallette colours to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     './app/**/*.js'
   ],
   theme: {
-    extend: {},
+    colors: {
+      "dark-green": "#013220",
+      "white": "#ffffff",
+      "black": "#000000",
+      "battleship-gray": "#798478",
+      "yinmn-blue": "#36558f"
+    },
+    extend: {}
   },
   plugins: [],
 }
